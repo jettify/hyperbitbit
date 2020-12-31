71b89c2c1c1a3b6b233862eb0e0a136f70605f50
Nikolay Novik <nickolainovik@gmail.com>
2020-12-30 21:27:52 -0500

    Use more stable rng (#8)

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,19 +1,19 @@
-extern crate hyperbitbit;
-extern crate rand;
-
 use hyperbitbit::HyperBitBit;
+use rand::Rng;
+use rand::SeedableRng;
 use rand::distributions::Alphanumeric;
-use rand::prelude::*;
+use rand_isaac::Isaac64Rng;
 use std::collections::HashSet;
+
 
 fn main() {
     let mut hbb = HyperBitBit::new();
     let mut items = HashSet::new();
-    let mut rng = StdRng::seed_from_u64(42);
+    let mut rng = Isaac64Rng::seed_from_u64(42);
 
-    let maxn = 10000;
+    let maxn = 100000;
     for _ in 1..maxn {
-        let s = (&mut rng).sample_iter(&Alphanumeric).take(4).collect::<String>();
+        let s = (&mut rng).sample_iter(&Alphanumeric).take(3).collect::<String>();
 
         hbb.insert(&s);
         items.insert(s);
@@ -21,7 +21,7 @@ fn main() {
     let expected: i64 = items.len() as i64;
     let rel: f64 = (100.0 * (expected - hbb.cardinality() as i64) as f64) / (expected as f64);
 
-    println!("Actuals cardinality:   {:?}", expected);
+    println!("Cardinality:           {:?}", expected);
     println!("Estimated cardinality: {:?}", hbb.cardinality());
     println!("Error % cardinality:   {:.2}", rel);
 }
